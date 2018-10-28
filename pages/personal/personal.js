e6352c47138fae0039439f399da62b9587970c35
yang4ever <2498727035@qq.com>
2018-10-28 15:53:47 +0800

    update

---
+++ pages/personal/personal.js
@@ -0,0 +1,88 @@
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 组件所需的参数
+    navbarData: {
+      showCapsule: 0, //是否显示左上角图标
+      title: '', //导航栏 中间的标题
+      color:"#85C7CA"
+    },
+    // 此页面 页面内容距最顶部的距离
+    height: app.globalData.height * 2 + 20,
+  },
+  toAttestation:function(){
+    wx.navigateTo({
+      url: '../attestation/attestation'　　// 个人认证
+    })
+  },
+  toMyPublish:function(){
+    wx.navigateTo({
+      url: '../mypublish/mypublish'　　// 我的发布
+    })
+  },
+  toTuCao:function(){
+    wx.navigateTo({
+      url: '../tucao/tucao'　　// 吐槽页面
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
